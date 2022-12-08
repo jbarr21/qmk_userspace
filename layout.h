d99fe3fd54a8a20c2f81000b829a2534d99e3e1e
James Barr <barr.james.r@gmail.com>
2022-12-08 00:02:33 -0800

    Fix 36 to 42 mapping macro

--- layout.h
+++ layout.h
@@ -154,10 +154,10 @@
     k21, k22, k23, k24, k25,    k26, k27, k28, k29, k30, \
               k31, k32, k33,    k34, k35, k36 \
 ) \
-    KC_NO,  k02, k03, k04, k05, k06,    k07, k08, k09, k10, k11, KC_BSLS, \
-    KC_ESC, k14, k15, k16, k17, k18,    k19, k20, k21, k22, k23, KC_QUOT, \
-    KC_NO,  k26, k27, k28, k29, k30,    k31, k32, k33, k34, k35, LLOCK,   \
-                      k37, k38, k39,    k40, k41, k42
+    KC_NO,  k01, k02, k03, k04, k05,    k06, k07, k08, k09, k10, KC_BSLS, \
+    KC_ESC, k11, k12, k13, k14, k15,    k16, k17, k18, k19, k20, KC_QUOT, \
+    KC_NO,  k21, k22, k23, k24, k25,    k26, k27, k28, k29, k30, LLOCK,   \
+                      k31, k32, k33,    k34, k35, k36
 
 // 3x5_3 to moonlander conversion
 #define C_ML(k) SPLIT_36_TO_ML(k)
